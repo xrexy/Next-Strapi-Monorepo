2d367e2bb300d6395b86e334bff262af693f8657
xrexy <rexydoesimportantthings@gmail.com>
2022-08-05 00:11:30 +0300

    Update for production (kinda)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,11 @@
-import type { GetStaticProps, NextPage } from "next";
+require("isomorphic-fetch");
+
+import type { GetServerSideProps, GetStaticProps, InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
 import { env } from "../env/client.mjs";
 import { InferGetStaticPropsType } from "next";
 
-function Home({ result }: InferGetStaticPropsType<typeof getStaticProps>) {
+function Home({ result }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Head>
@@ -12,18 +14,20 @@ function Home({ result }: InferGetStaticPropsType<typeof getStaticProps>) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1>Heavy development  atm :(</h1>
+        <h1>Heavy development atm :(</h1>
 
         <ul>
           {result &&
-            result.map(({ attributes: { title }, id }) => <li key={id}>{title}</li>)}
+            result.map(({ attributes: { title }, id }) => (
+              <li key={id}>{title}</li>
+            ))}
         </ul>
       </div>
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps<
+export const getServerSideProps: GetServerSideProps<
   StrapiResponse<Movie>
 > = async () => {
   return fetch(`${env.NEXT_PUBLIC_API_URL}/movies`, {
@@ -32,15 +36,21 @@ export const getStaticProps: GetStaticProps<
       "Content-Type": "application/json",
     },
   })
-    .then((res) => res.json())
+    .then((res) => {
+      if (res.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return res.json();
+    })
     .then((data) => {
+      console.log(data);
       return {
         props: {
           meta: data.meta,
           result: data.data,
         },
       };
-    });
+    })
 };
 
 export default Home;
